82df675559acce3b7090411ac54bb201402e20ed
shauniif <francescavescio12@gmail.com>
2024-07-19 09:00:10 +0200

    Aggiungere i file di progetto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROGETTO_SETTIMINALE_BE_S5_L5__Vescio_Pia_Francesca.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
